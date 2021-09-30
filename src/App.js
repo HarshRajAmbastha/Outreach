989a1b73a30ef0631a78e4a5662a853386e54ce3
HarshRajAmbastha <harsh_ra@ce.iitr.ac.in>
2021-09-30 23:16:17 +0530

    added01

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Scheme from './Scheme'
+const App = () => {
+  return (
+    <div>
+      <Scheme/>
+ 
+    </div>
+  )
+}
+
+export default App
+
---
+++ src/Scheme.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import "./scheme.css"
+
+const Scheme = () => {
+    return (
+        <>
+        <div className="schemeR1">
+          <div className="icon">
+          <img src="https://media.istockphoto.com/vectors/startup-icon-vector-id1074164928?k=20&m=1074164928&s=612x612&w=0&h=dsjGDvOJrdI5UWC6YBCbw31Ue7n1d3WqjjS8RRba920="alt="" />
+          </div>
+          <div className="number"><span>No.1</span></div>
+          <div className="icon">
+              <img src="https://media.istockphoto.com/vectors/startup-icon-vector-id1074164928?k=20&m=1074164928&s=612x612&w=0&h=dsjGDvOJrdI5UWC6YBCbw31Ue7n1d3WqjjS8RRba920="alt="" />
+          </div>
+          <div className="number"><span>4+</span></div>
+          <div className="icon">
+          <img src="https://media.istockphoto.com/vectors/startup-icon-vector-id1074164928?k=20&m=1074164928&s=612x612&w=0&h=dsjGDvOJrdI5UWC6YBCbw31Ue7n1d3WqjjS8RRba920="alt="" />
+          </div>
+          <div className="number"><span>390+</span></div>
+          <div className="icon">
+          <img src="https://media.istockphoto.com/vectors/startup-icon-vector-id1074164928?k=20&m=1074164928&s=612x612&w=0&h=dsjGDvOJrdI5UWC6YBCbw31Ue7n1d3WqjjS8RRba920="alt="" />
+          </div>
+          <div className="number"><span>390+</span></div>
+        </div>
+        <div className="schemeR2">
+        <div className="text">Architecture and Planning in the country</div>
+        <div className="text">Scholarship schemes</div>
+        <div className="text">Startups</div>
+        <div className="text">Startups</div>
+        </div>
+        </>
+    )
+}
+
+export default Scheme
